ffd895cfd688575c68e7356b2d7976549c7d5df4
khurshid <Khurshid@qurilo.com>
2024-09-11 11:09:32 +0530

    update in deals and leadroutes

--- controllers/deals.js
+++ controllers/deals.js
@@ -3,7 +3,7 @@ const dealModel = require("../models/dealSchema");
 
 exports.dealController = async (req, res, next) => {
   try {
-    const { leadId } = req.body;
+    const { leadId, stage } = req.body; // we will send the stage from frontend or by default it would be sent as Qualified
 
     // query the leads collection
     const lead = await mongoose.connection
@@ -13,19 +13,41 @@ exports.dealController = async (req, res, next) => {
     console.log(lead, "haha");
 
     if (!lead) {
-      return res.status(404).json({ message: "Lead not found"});
+      return res.status(404).json({ message: "Lead not found" });
     }
 
     // create a deal by referencing the lead
-
     const newDeal = new dealModel({
       leadId: lead._id,
-      stage: "Qualified",
+      stage,
     });
 
     await newDeal.save();
 
     res.json({ message: "lead converted to deal succesfully ", deal: newDeal });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// controller to update the stage
+
+exports.updateDealStage = async (req, res, next) => {
+  try {
+    const { dealId, stage } = req.body;
+
+    // update the deal stage directly
+    const updateDeal = await dealModel.findByIdAndUpdate(
+      dealId,
+      { stage }, // mongoose will validate the stage here
+      { new: true, runValidators: true }
+    );
+
+    if (!updateDeal) {
+      return res.status(404).json({ message: "Deal not found" });
+    }
+
+    res.json({ message: "deal stage updated successfully", deal: updateDeal });
   } catch (error) {
     next(error);
   }
--- routes/leadsRoutes.js
+++ routes/leadsRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { dealController } = require("../controllers/deals");
+const { dealController, updateDealStage } = require("../controllers/deals");
 
 const router = express.Router();
 
 // route to convert the lead to deal
 
 router.post("/deals", dealController);
-
+router.post("/deals/update", updateDealStage);
 
 module.exports = router;
